6a823238c594f31a95d84956553d8f7895ee9164
shotboy <jw1234>
2019-12-18 23:52:02 +0800

    test_client

--- tests/test_client.py
+++ tests/test_client.py
@@ -6,6 +6,7 @@
 @Motto：I have a bad feeling about this.
 """
 from exchanges.Deribit.deribit import ExchGwTradeDeribit
+from exchanges.gateway import ExchangeGateway
 from util.instrument import Instrument
 from util.Log import Logger
 from clients.zmqClient import ZmqClient
@@ -16,9 +17,13 @@ import ast
 Logger.init_log()
 dbclient = ZmqClient()
 dbclient.connect(addr='tcp://127.0.0.1:5000')
+
+ExchangeGateway.init_snapshot_table([dbclient])
+
 exchange_name = 'Deribit'
 instmt_name = 'BTC'
 instmt_code = 'BTC-20DEC19-7250-C'
 instmt = Instrument(exchange_name, instmt_name, instmt_code)
+
 exch = ExchGwTradeDeribit([dbclient])
 exch.start(instmt)
